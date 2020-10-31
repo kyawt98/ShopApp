f77e5156425ff0fe2049908bfcba8fb24673f88c
Your Name <kyawtkyawtsan1998@gmail.com>
2020-10-31 21:46:22 +0630

    sth

--- lib/provider/products.dart
+++ lib/provider/products.dart
@@ -5,14 +5,14 @@ class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
         id: 'p1',
-        title: 'Ultra Soft Fleece Midweight Casual Solid Cargo Jogger Pants ',
+        title: 'Midweight Casual Solid Cargo Jogger Pants ',
         description: ' Drawstring closure ',
         price: 22.0,
         imgUrl:
             'https://images-na.ssl-images-amazon.com/images/I/71MCpvYk7FL._AC_SY500._SX._UX._SY._UY_.jpg'),
     Product(
         id: 'p2',
-        title: ' Starter Women Soccer Pants, Prime Exclusive ',
+        title: 'Starter Women Soccer Pants',
         description: ' 100% Polyester  ',
         price: 22.86,
         imgUrl:
@@ -20,7 +20,7 @@ class Products with ChangeNotifier {
     Product(
         id: 'p3',
         title:
-            'Amazon Brand - Core 10 Women (XS-3X) Motion Tech Fleece Relaxed Fit Jogger Sweatpant  ',
+            'Motion Tech Fleece Relaxed Fit Jogger Sweatpant  ',
         description: ' 66% Cotton, 34% Polyester ',
         price: 37.10,
         imgUrl:
@@ -28,14 +28,14 @@ class Products with ChangeNotifier {
     Product(
         id: 'p4',
         title:
-            'Amazon Brand - Mae Women Sleepwear Marshmallow Fleece Pullover Top and Jogger Pajama Set  ',
+            'Sleepwear Marshmallow Fleece Pullover Top and Jogger Pajama Set  ',
         description: '  100% Polyester  ',
         price: 22.04,
         imgUrl:
             'https://m.media-amazon.com/images/I/81cyHFk+NfL._MCnd_SEARCH281250_AC_UL320_.jpg'),
     Product(
         id: 'p5',
-        title: 'Amazon Brand - Goodthreads Women Denim Cargo Jeans  ',
+        title: 'Goodthreads Women Denim Cargo Jeans  ',
         description: '  92% Cotton, 4% Polyester, 4% Elastane  ',
         price: 45.0,
         imgUrl:
@@ -43,14 +43,14 @@ class Products with ChangeNotifier {
     Product(
         id: 'p6',
         title:
-            'The Drop Women Greta Fitted Square Neck Halter Sweater Bralette',
+            'Greta Fitted Square Neck Halter Sweater Bralette',
         description: ' 65% Viscose, 35% Nylon-Spandex ',
         price: 29.90,
         imgUrl:
             'https://m.media-amazon.com/images/I/811JsLV7f2L._AC_UL320_.jpg'),
     Product(
         id: 'p7',
-        title: 'The Drop Women Dina Fleece Elastic Hem Sweatpant  ',
+        title: 'Dina Fleece Elastic Hem Sweatpant  ',
         description: '  60% Cotton, 40% Polyester ',
         price: 44.90,
         imgUrl:
--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,10 +1,19 @@
 import 'package:intl/intl.dart';
 import '../provider/orders.dart' as ord;
 import 'package:flutter/material.dart';
-class OrderItem extends StatelessWidget {
+import 'dart:math';
+
+class OrderItem extends StatefulWidget {
   final ord.OrderItem orders;
 
   OrderItem(this.orders);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expended = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +22,46 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${orders.amount}'),
+            title: Text('\$${widget.orders.amount}'),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: (){},
+              icon: Icon(_expended ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expended = !_expended;
+                });
+              },
             ),
-            subtitle: Text(DateFormat('dd MM yyyy hh:mm').format(orders.dateTime)),
-          )
+            subtitle: Text(
+                DateFormat('dd MM yyyy hh:mm').format(widget.orders.dateTime)),
+          ),
+          if (_expended)
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+              height: min(widget.orders.products.length * 20 + 20.0, 150),
+              width: double.infinity,
+              child: ListView(
+                  children: widget.orders.products
+                      .map((product) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              FittedBox(
+                                child: Text(
+                                  product.title,
+                                  style: TextStyle(
+                                      fontSize: 16, ),
+                                ),
+                              ),
+                              Text(
+                                '${product.quantity} x \$${product.price}',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14,
+                                ),
+                              )
+                            ],
+                          ))
+                      .toList()),
+            )
         ],
       ),
     );
